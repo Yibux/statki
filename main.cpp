b103e2766e72d602ffe2055ba792c7eaa49baf02
Jakub Szewczyk <240810@edu.p.lodz.pl>
2023-06-08 19:05:04 +0200

    poprawa wielkosci mapy

--- main.cpp
+++ main.cpp
@@ -9,20 +9,24 @@ using namespace std;
 class Player {
 public:
     static const int BOARD_SIZE = 30;
+    static const int BOARD_SIZE_X = 15;
     bool komputer = false;
+    int xTrafienie = -1, yTrafienie = -1;
 
-    char plansza[BOARD_SIZE][BOARD_SIZE];
+    char plansza[BOARD_SIZE_X][BOARD_SIZE];
+    char planszaDoStrzelania[BOARD_SIZE_X][BOARD_SIZE];
 
     Player() {
-        for (int i = 0; i < BOARD_SIZE; ++i) {
+        for (int i = 0; i < BOARD_SIZE_X; ++i) {
             for (int j = 0; j < BOARD_SIZE; ++j) {
                 plansza[i][j] = '*';
+                planszaDoStrzelania[i][j] = '*';
             }
         }
     }
 
     void wyswietlPlansze() {
-        for (int i = 0; i < BOARD_SIZE; ++i) {
+        for (int i = 0; i < BOARD_SIZE_X; ++i) {
             for (int j = 0; j < BOARD_SIZE; ++j) {
                 cout << plansza[i][j] << " ";
             }
@@ -31,7 +35,7 @@ public:
     }
 
     bool czyKoordynatyPoprawne(int x, int y) {
-        return (x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE);
+        return (x >= 0 && x < BOARD_SIZE_X && y >= 0 && y < BOARD_SIZE);
     }
 
     bool czyPolePuste(int x, int y) {
@@ -45,7 +49,7 @@ public:
         if (orientacja == 1 && y + dlugoscStatku >= BOARD_SIZE)
             return false;
 
-        if (orientacja == 0 && x + dlugoscStatku >= BOARD_SIZE)
+        if (orientacja == 0 && x + dlugoscStatku >= BOARD_SIZE_X)
             return false;
 
         for (int i = 0; i < dlugoscStatku; ++i) {
@@ -61,7 +65,7 @@ public:
 
     bool postawStatek(int x, int y, int orientacja, int dlugoscStatku) {
         if (!czyKoordynatyPoprawne(x, y) || !czyMoznaPostawicStatek(x, y, orientacja, dlugoscStatku)) {
-            cout << "Nie można postawić statku na tych koordynatach." << endl;
+            cout << "Nie mozna postawic statku na tych koordynatach." << endl;
             return false;
         }
 
@@ -84,10 +88,10 @@ public:
             while(true) {
                 orientacja = rand() % 2;
                 if(orientacja == 0) {
-                    x = rand() % BOARD_SIZE;
+                    x = rand() % BOARD_SIZE_X;
                     y = rand() % BOARD_SIZE - dlugosc;
                 } else {
-                    x = rand() % BOARD_SIZE-dlugosc;
+                    x = rand() % BOARD_SIZE_X - dlugosc;
                     y = rand() % BOARD_SIZE;
                 }
                 if(czyMoznaPostawicStatek(x, y, orientacja, dlugosc))
@@ -118,7 +122,7 @@ public:
             bool poprawneKoordynaty = false;
             int x, y, orientacja;
 
-            while (!poprawneKoordynaty) {
+            do {
                 cout << "Podaj koordynaty dla statku o dlugosci " << dlugosc << ":" << endl;
                 cout << "Wprowadz x: ";
                 cin >> x;
@@ -134,9 +138,8 @@ public:
                 } else {
                     cout << "Niepoprawne koordynaty. Wprowadz je ponownie." << endl;
                 }
-            }
+            } while (!players[gracz].postawStatek(x, y, orientacja, dlugosc));
 
-            players[gracz].postawStatek(x, y, orientacja, dlugosc);
             players[gracz].wyswietlPlansze();
         }
 
@@ -153,15 +156,36 @@ public:
 
         while (true) {
             cout << "Gracz " << aktualnyGracz << ", twoj ruch." << endl;
-            cout << "Wprowadz koordynaty strzalu (x, y): ";
+            if(!players[1 - aktualnyGracz].komputer)
+                cout << "Wprowadz koordynaty strzalu (x, y): ";
             int x, y;
             if(!players[1 - aktualnyGracz].komputer)
                 cin >> y >> x;
             else {
-                srand(std::chrono::system_clock::now().time_since_epoch().count());
-                x = rand() % Player::BOARD_SIZE;
-                y = rand() % Player::BOARD_SIZE;
+                if(players[1 - aktualnyGracz].xTrafienie != -1) {
+                    int newX = players[1 - aktualnyGracz].xTrafienie;
+                    int newY = players[1 - aktualnyGracz].yTrafienie;
+                    if(newX - 1 >= 0) {
+                        x = newX - 1;
+                        y = newY;
+                    } else if(newY + 1 <= Player::BOARD_SIZE - 1) {
+                        x = newX;
+                        y = newY + 1;
+                    } else if(newX + 1 <= Player::BOARD_SIZE_X - 1) {
+                        x = newX + 1;
+                        y = newY;
+                    } else {
+                        x = newX;
+                        y = newY - 1;
+                    }
+                }
+                else {
+                    srand(std::chrono::system_clock::now().time_since_epoch().count());
+                    x = rand() % Player::BOARD_SIZE_X;
+                    y = rand() % Player::BOARD_SIZE;
+                }
                 sleep(1);
+                cout << "Koordynaty: " << x << " " << y << endl;
             }
 
             if (!players[1 - aktualnyGracz].czyKoordynatyPoprawne(x, y)) {
@@ -169,22 +193,24 @@ public:
                 continue;
             }
 
-            char pole = players[1 - aktualnyGracz].plansza[x][y];
-            if (pole == 'X' || pole == 'O') {
+            char pole = players[1 - aktualnyGracz].planszaDoStrzelania[x][y];
+            char polePlanszy = players[1 - aktualnyGracz].plansza[x][y];
+            if (polePlanszy == 'X' || pole == 'O') {
                 cout << "To pole zostalo juz trafione." << endl;
                 continue;
             }
 
             if (pole == '*') {
                 cout << "Pudlo!" << endl;
-                players[1 - aktualnyGracz].plansza[x][y] = 'O';
+                players[1 - aktualnyGracz].planszaDoStrzelania[x][y] = 'O';
             }
-            else {
+            else if(polePlanszy) {
                 int dlugoscStatku = pole - '0';
                 players[1 - aktualnyGracz].plansza[x][y] = 'X';
+                players[1 - aktualnyGracz].planszaDoStrzelania[x][y] = 'X';
 
                 bool zatopiony = true;
-                for (int i = 0; i < Player::BOARD_SIZE; ++i) {
+                for (int i = 0; i < Player::BOARD_SIZE_X; ++i) {
                     for (int j = 0; j < Player::BOARD_SIZE; ++j) {
                         if (players[1 - aktualnyGracz].plansza[i][j] == dlugoscStatku + '0') {
                             zatopiony = false;
@@ -195,16 +221,22 @@ public:
 
                 if (zatopiony) {
                     cout << "Trafiony zatopiony!" << endl;
+                    players[1 - aktualnyGracz].xTrafienie = -1;
+                    players[1 - aktualnyGracz].yTrafienie = -1;
+                    //TODO: resetowac punkt trafienia
                 }
                 else {
                     cout << "Trafiony!" << endl;
+                    players[1 - aktualnyGracz].xTrafienie = x;
+                    players[1 - aktualnyGracz].yTrafienie = y;
+                    //TODO: dorobić mechanizm do lepszego trafiania gdy cos zostanie tu trafione
                 }
             }
 
             players[1 - aktualnyGracz].wyswietlPlansze();
 
             bool czyKoniecGry = true;
-            for (int i = 0; i < Player::BOARD_SIZE; ++i) {
+            for (int i = 0; i < Player::BOARD_SIZE_X; ++i) {
                 for (int j = 0; j < Player::BOARD_SIZE; ++j) {
                     if (players[1 - aktualnyGracz].plansza[i][j] >= '2' && players[1 - aktualnyGracz].plansza[i][j] <= '5') {
                         czyKoniecGry = false;
